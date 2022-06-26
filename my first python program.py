6c9bd4c7eada6fcc5fe9e0f7fedc6f261e0939b8
Gooey Code <106923499+gooeycode@users.noreply.github.com>
2022-06-26 22:28:19 +0100

    my first python program
    boom shakalaka

---
+++ my first python program.py
@@ -0,0 +1,5 @@
+print("Hello & welcome to my first python program.")
+nameInput = input("Whats your name?")
+input = input("Hi " + nameInput +". How old are you?")
+print("You are " + input + " years old.")
+print("You've joined the programming party at just the right time. Welcome :)")
